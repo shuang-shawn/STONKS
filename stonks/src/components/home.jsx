e700dcc5fb617d865603c518f6268f5abcf9b399
Niki Guo <qguo20@my.bcit.ca>
2021-12-17 14:21:39 -0800

    Create server folder for bot

--- stonks/src/components/home.jsx
+++ stonks/src/components/home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./home.css";
 
 function Home() {
   return (
@@ -15,10 +16,8 @@ function Home() {
           <div class="col-lg-5">
             <h1 class="font-weight-light">Home</h1>
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
+              This is the homepage of where your stonk overview will be. 
+
             </p>
           </div>
         </div>
