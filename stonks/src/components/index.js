04fab40a721803c406e68e6121c6932bca9f0068
snyoon <justinyoon23@gmail.com>
2021-12-18 09:45:23 -0800

    new project structure

--- stonks/src/components/index.js
+++
@@ -1,9 +0,0 @@
-export { default as Navigation } from "./navigation";
-export { default as Footer } from "./footer";
-export { default as Home } from "./home";
-export { default as About } from "./about";
-export { default as Contact } from "./contact";
-export { default as Blog } from "./blog/Blog";
-export { default as Posts } from "./blog/Posts";
-export { default as Post } from "./blog/Post";
-
