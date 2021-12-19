7f0ce06cf8fdd628bbc18d1d64498bb8d6841af8
Niki Guo <qguo20@my.bcit.ca>
2021-12-18 20:32:07 -0800

    Add algo description

--- src/__mocks__/models.js
+++ src/__mocks__/models.js
@@ -3,21 +3,21 @@ import { v4 as uuid } from 'uuid';
 export const models = [
   {
     id: uuid(),
-    name: 'model1',
-    description: 'Description for model 1. Description for model 1. Description for model 1. Description for model 1. ',
-    media: '/static/images/products/product_1.png',
+    name: 'Low',
+    description: 'Dollar Cost Averaging Algorithm - This algorithm is for users who like a fine balance. The basic idea is to break down a large order into small orders and place them in the market over time. The logic behind it is that price fluctuates many times, and you may buy the stock cheaper overall compared to just investing in the stock at one point in time. works well in long term ',
+    media: '/static/images/products/product_2.png',
   },
   {
     id: uuid(),
-    name: 'model2',
-    description: 'Description for model 2. Description for model 2. Description for model 2. Description for model 2. ',
-    media: '/static/images/products/product_1.png',
+    name: 'Medium',
+    description: 'Moving Average Algorithm - This algorithm is for users prefer to play it safe. Simple moving average crossover, which buys a stock if its short-period moving average value surpasses its long-period moving average value, and sells if the inverse event happens.                                                                                                                    ',
+    media: '/static/images/products/product_3.png',
   },
   {
     id: uuid(),
-    name: 'model3',
-    description: 'Description for model 3. Description for model 3. Description for model 3. Description for model 3. ',
-    media: '/static/images/products/product_1.png',
+    name: 'High',
+    description: ' RSI Algorithm - This algorithm is for users who are more risk tolerant. High risk, high reward. It measures the speed and change of price movements using a scale of 0 to 100.  a higher RSI value is said to indicate an overbought asset (time to sell) while a lower RSI value is said to indicate an oversold asset (time to buy)                                                  ',
+    media: '/static/images/products/product_4.png',
   },
   
-];
+];    
